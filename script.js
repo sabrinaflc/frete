9f5b4358793141626a85f989a3be85779c01e57d
Sabrina Lins <83469094+sabrinaflc@users.noreply.github.com>
2023-08-29 19:00:44 -0300

    script.js

--- script.js
+++ script.js
@@ -5,33 +5,61 @@ document.addEventListener("DOMContentLoaded", function () {
     const calcularFreteButton = document.getElementById("calcular-frete");
     const resultadoFrete = document.getElementById("resultado-frete");
 
-    calcularFreteButton.addEventListener("click", function () {
+    cepOrigemInput.addEventListener('input', function() {
+        const cepValue = cepOrigemInput.value.trim();
+        
+        if (cepValue.length === 5) {
+            cepOrigemInput.value = cepValue.slice(0, 5) + '-' + cepValue.slice(5);
+        }
+    });
+
+    cepDestinoInput.addEventListener('input', function() {
+        const cepValue = cepDestinoInput.value.trim();
+        
+        if (cepValue.length === 5) {
+            cepDestinoInput.value = cepValue.slice(0, 5) + '-' + cepValue.slice(5);
+        }
+    });
+
+    calcularFreteButton.addEventListener("click", async function () {
         const cepOrigem = cepOrigemInput.value;
         const cepDestino = cepDestinoInput.value;
 
-        // Aqui você pode usar uma API de cálculo de frete para obter a cotação com base nos CEPs de origem e destino
-        // Substitua a URL pela API de cálculo de frete que você está usando
-        const url = `https://api-de-calcular-frete.com/calcular?origem=${cepOrigem}&destino=${cepDestino}`;
+        // URL da API ViaCEP
+        const urlOrigem = `https://viacep.com.br/ws/${cepOrigem}/json/`;
+        const urlDestino = `https://viacep.com.br/ws/${cepDestino}/json/`;
 
-        // Aqui, você pode fazer uma solicitação HTTP para a API de cálculo de frete usando fetch() ou outra biblioteca
-        // e obter a cotação de frete.
+        try {
+            // Fazendo a solicitação HTTP para a API ViaCEP para obter informações dos CEPs
+            const responseOrigem = await fetch(urlOrigem);
+            const responseDestino = await fetch(urlDestino);
 
-        // Vamos simular uma resposta aqui para fins de demonstração:
-        const frete = {
-            origem: "12345-678",
-            destino: "98765-432",
-            valor: 25.0, // Valor do frete em reais
-            prazo: 5 // Prazo de entrega em dias
-        };
+            const dataOrigem = await responseOrigem.json();
+            const dataDestino = await responseDestino.json();
 
-        // Exibir o resultado na página
-        resultadoFrete.innerHTML = `
-            <p>Origem: ${frete.origem}</p>
-            <p>Destino: ${frete.destino}</p>
-            <p>Valor do Frete: R$ ${frete.valor.toFixed(2)}</p>
-            <p>Prazo de Entrega: ${frete.prazo} dias</p>
-        `;
+            // Verificando se os CEPs estão na mesma cidade
+            const mesmaCidade = dataOrigem.localidade === dataDestino.localidade;
 
-        resultadoFrete.style.display = "block";
+            // Definindo o tempo de entrega com base na cidade
+            let tempoDeEntrega = mesmaCidade ? "Entre 1 e 2 dias" : "10 dias";
+
+            // Calculando o valor do frete com base no tempo de entrega
+            const valorBase = 2.0;
+            const acrescimoPorDia = 5.0;
+
+            const valorDoFrete = valorBase + acrescimoPorDia * (mesmaCidade ? 1 : 10);
+
+            // Exibir o resultado na página
+            resultadoFrete.innerHTML = `
+                <p>Origem: ${dataOrigem.logradouro}, ${dataOrigem.localidade} - ${dataOrigem.uf}</p>
+                <p>Destino: ${dataDestino.logradouro}, ${dataDestino.localidade} - ${dataDestino.uf}</p>
+                <p>Tempo de Entrega: ${tempoDeEntrega}</p>
+                <p>Valor do Frete: R$ ${valorDoFrete.toFixed(2)}</p>
+            `;
+
+            resultadoFrete.style.display = "block";
+        } catch (error) {
+            console.error("Erro ao obter informações dos CEPs:", error);
+        }
     });
 });
